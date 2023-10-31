abe54c42a20f24285b7ae1c5d337c2a1079d56d4
peruibeloko <cmendonca32@gmail.com>
2023-10-31 17:38:31 -0300

    start add content listing by month and year

---
+++ pages/blog/[year]/[month].vue
@@ -0,0 +1,9 @@
+<script setup>
+definePageMeta({
+  name: 'month-listing',
+  path: '/blog/:year(\\d{4})/:month(0[1-9]|1[0-2])'
+})
+</script>
+<template>
+  Month listing for {{ $route.params.month }} of {{ $route.params.year }}
+</template>
---
+++ pages/blog/[year]/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+definePageMeta({
+  name: 'year-listing',
+  path: '/blog/:year(\\d{4})'
+})
+</script>
+<template>
+  Year listing for {{ $route.params.year }}
+</template>
