4392859fdc862278ea32af8980250aa0c59ef072
peruibeloko <cmendonca32@gmail.com>
2024-04-11 19:00:52 -0300

    start refactor styling

---
+++ pages/resume.vue
@@ -0,0 +1,19 @@
+<script setup>
+definePageMeta({
+  name: "Resume"
+});
+</script>
+
+<template>
+  <div id="about">
+    <header>
+      <h1>Currículo</h1>
+      <NuxtLink to="/">Home</NuxtLink>
+    </header>
+    <main>
+      <ContentDoc tag="article" />
+    </main>
+  </div>
+</template>
+
+<style src="~/assets/css/resume.scss" />
